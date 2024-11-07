3164610147b12466d1681240d0f5d9eeec25d45a
string-TIRTH <string-TIRTH>
2024-11-07 19:23:03 +0530

    initial commit

---
+++ src/main/java/com/oea/online_exam_app/OnlineExamAppApplication.java
@@ -0,0 +1,13 @@
+package com.oea.online_exam_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oea/online_exam_app/OnlineExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oea.online_exam_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
